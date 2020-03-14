6e77d70859ec7ba656db7b0a1216e8009a6a2c9a
WangWilliams <1179574672@qq.com>
2020-03-14 15:10:57 +0800

    类继承方式编写适配器

---
+++ src/com/company/adapter/AdapteeTest.java
@@ -0,0 +1,28 @@
+package com.company.adapter;
+
+interface Target {
+    public void Request();
+}
+
+/**
+ * 类继承的写法
+ */
+public class AdapteeTest {
+    public static void main(String[] args) {
+        System.out.println("适配器模拟测试。");
+        Target target = new ClassAdapter();
+        target.Request();
+    }
+}
+
+class Adaptee {
+    public void specialRequest() {
+        System.out.println("适配器中的代码被调用！");
+    }
+}
+
+class ClassAdapter extends Adaptee implements Target {
+    public void Request() {
+        super.specialRequest();
+    }
+}
